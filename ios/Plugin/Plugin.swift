a80067abeea1e07f9288d107f99c80f34038cb29
Chris Woolum <christopher.woolum@gmail.com>
2020-02-27 12:15:11 -0800

    begin ios

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -8,10 +8,43 @@ import Capacitor
 @objc(ContactManager)
 public class ContactManager: CAPPlugin {
     
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.success([
-            "value": value
-        ])
+    @objc func getContacts(_ call: CAPPluginCall) {
+        let query = call.getString("query") ?? ""
+        
+    }
+
+    func filterContacts(query: String) {
+        let contactStore = CNContactStore()
+        
+        let keysToFetch = [
+            CNContactFormatter.descriptorForRequiredKeysForStyle(.FullName),
+            CNContactEmailAddressesKey,
+            CNContactPhoneNumbersKey,
+            CNContactImageDataAvailableKey,
+            CNContactThumbnailImageDataKey]
+
+        // Get all the containers
+        var allContainers: [CNContainer] = []
+        do {
+            allContainers = try contactStore.containersMatchingPredicate(nil)
+        } catch {
+            print("Error fetching containers")
+        }
+
+        var results: [CNContact] = []
+
+        // Iterate all containers and append their contacts to our results array
+        for container in allContainers {
+            let fetchPredicate = CNContact.predicateForContactsInContainerWithIdentifier(container.identifier)
+
+            do {
+                let containerResults = try contactStore.unifiedContactsMatchingPredicate(fetchPredicate, keysToFetch: keysToFetch)
+                results.appendContentsOf(containerResults)
+            } catch {
+                print("Error fetching results for container")
+            }
+        }
+
+        return results
     }
 }
